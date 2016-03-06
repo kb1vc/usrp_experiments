d706e16b3dd4d354b0506cc47ca7500bfa3b67e8
Matt Reilly <kb1vc@kb1vc.org>
2016-03-06 16:50:15 -0500

    It works!  Two tones....

--- B210TwoTone/B210TwoTone.cxx
+++ B210TwoTone/B210TwoTone.cxx
@@ -21,6 +21,7 @@
 
 #include <uhd/utils/thread_priority.hpp>
 #include <uhd/utils/safe_main.hpp>
+#include <uhd/utils/log.hpp>
 #include <uhd/usrp/multi_usrp.hpp>
 #include <uhd/exception.hpp>
 #include <boost/program_options.hpp>
@@ -81,6 +82,18 @@ int main(int argc, char *argv[]){
     //Lock mboard clocks
     usrp->set_clock_source(ref);
 
+    uhd::usrp::subdev_spec_t twoports("A:A A:B");
+    usrp->set_tx_subdev_spec(twoports);
+
+    //create a transmit streamer
+    //linearly map channels (index0 = channel0, index1 = channel1, ...)
+    uhd::stream_args_t stream_args("fc32", "sc8");
+    std::vector<size_t> channel_nums; 
+    channel_nums.push_back(0);     channel_nums.push_back(1); 
+    stream_args.channels = channel_nums;
+    uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
+
+    
     std::cout << boost::format("Using Device: %s") % usrp->get_pp_string() << std::endl;
 
     rate = 625000;
@@ -124,27 +137,27 @@ int main(int argc, char *argv[]){
 
     int i; 
     for(i = 0; i < 2; i++) {
+      UHD_LOG << boost::format("!!!!Setting TX freq [%d] to rf=%16.14g dsp=%16.14g\n")
+	% i % trs[i].rf_freq % trs[i].dsp_freq; 
       tres = usrp->set_tx_freq(trs[i], i);
       std::cout << boost::format("\nChan[%d] Actual TX Freq: %f MHz...\n") % i % (usrp->get_tx_freq(i) * 1e-6);
       std::cout << boost::format("\tRF_actual = %16.14g MHz  DUC = %g Hz target = %16.14g MHz\n")
+	% (tres.actual_rf_freq * 1e-6)% tres.actual_dsp_freq % (usrp->get_tx_freq(i) * 1e-6);
+
+      UHD_LOG << boost::format("\nChan[%d] Actual TX Freq: %f MHz...\n") % i % (usrp->get_tx_freq(i) * 1e-6);
+      UHD_LOG << boost::format("\tRF_actual = %16.14g MHz  DUC = %g Hz target = %16.14g MHz\n")
 	% (tres.actual_rf_freq * 1e-6)% tres.actual_dsp_freq % (usrp->get_tx_freq(i) * 1e-6);
     }
     
     for(i = 0; i < 2; i++) {
       double ff = usrp->get_tx_freq(i); 
       std::cout << boost::format("Check -- channel %d freq = %16.4g MHz\n") % i % (ff * 1e-6);
+      UHD_LOG << boost::format("Check -- channel %d freq = %16.4g MHz\n") % i % (ff * 1e-6);      
     }
     
 
     boost::this_thread::sleep(boost::posix_time::seconds(1)); //allow for some setup time
 
-    //create a transmit streamer
-    //linearly map channels (index0 = channel0, index1 = channel1, ...)
-    uhd::stream_args_t stream_args("fc32", "sc8");
-    std::vector<size_t> channel_nums; 
-    channel_nums.push_back(0);     channel_nums.push_back(1); 
-    stream_args.channels = channel_nums;
-    uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
 
     //set the rf gain
     if (vm.count("gain")){
@@ -171,11 +184,7 @@ int main(int argc, char *argv[]){
         UHD_ASSERT_THROW(lo_locked.to_bool());
     }
     sensor_names = usrp->get_mboard_sensor_names(0);
-    if ((ref == "mimo") and (std::find(sensor_names.begin(), sensor_names.end(), "mimo_locked") != sensor_names.end())) {
-        uhd::sensor_value_t mimo_locked = usrp->get_mboard_sensor("mimo_locked",0);
-        std::cout << boost::format("Checking TX: %s ...") % mimo_locked.to_pp_string() << std::endl;
-        UHD_ASSERT_THROW(mimo_locked.to_bool());
-    }
+
     if ((ref == "external") and (std::find(sensor_names.begin(), sensor_names.end(), "ref_locked") != sensor_names.end())) {
         uhd::sensor_value_t ref_locked = usrp->get_mboard_sensor("ref_locked",0);
         std::cout << boost::format("Checking TX: %s ...") % ref_locked.to_pp_string() << std::endl;
@@ -183,18 +192,17 @@ int main(int argc, char *argv[]){
     }
 
     // Set up metadata. We start streaming a bit in the future
-    // to allow MIMO operation:
     uhd::tx_metadata_t md;
     md.start_of_burst = true;
     md.end_of_burst   = false;
     md.has_time_spec  = true;
-    md.time_spec = usrp->get_time_now() + uhd::time_spec_t(0.1);
+    md.time_spec = usrp->get_time_now() + uhd::time_spec_t(0.5);
     
     //fill the buffer with a constant waveform.
     for (size_t n = 0; n < buff.size(); n++){
-      buff[n] = 1.0;
+      buff[n] = std::complex<float>(ampl,ampl);
     }
-
+    std::cerr << "*****" << std::endl; 
     while(1) {
       tx_stream->send(buffs, buff.size(), md);
       md.start_of_burst = false;
--- B210TwoTone/README.md
+++ B210TwoTone/README.md
@@ -6,9 +6,11 @@ For instance, try this:
 cd build
 cmake ../
 make
-./B210TwoTone  --freq 5760.01e6 --sep .1e6 --ampl 0.3 --gain 70 
+./B210TwoTone  --freq 5760.01e6 --sep .1e6 --ampl 0.3 --gain 80 
 ~~~~~
 
 Then look at the output from the B200 on a spectrum analyzer.  You should
 see two carriers near 5760.01 MHz and a spurious product at 5760.11 MHz
+
+The max separation seems to be about 15 MHz or so...
 
